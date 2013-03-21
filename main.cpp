f57a07127426bb1e8bba2d9b37c496ad9d496308
Linhua Tan <toolchainX@gmail.com>
2013-03-21 16:14:58 +0800

    can show the wrong volume rendering result.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// 首先绘制一个正方体
 #include <fstream>
 #include <iostream>
 #include <cstdio>
@@ -12,15 +11,28 @@
 #include <GL/glm/gtx/transform2.hpp>
 #include <GL/glm/gtc/type_ptr.hpp>
 
+
+#define GL_ERROR() checkForOpenGLError(__FILE__, __LINE__)
 using namespace std;
 using glm::mat4;
 using glm::vec3;
-int g_vao;
-int g_programHandle;
-int g_winWidth = 400;
-int g_winHeight = 400;
-int g_angle = 0;
-
+GLuint g_vao;
+GLuint g_programHandle;
+GLuint g_winWidth = 400;
+GLuint g_winHeight = 400;
+GLint g_angle = 0;
+GLuint g_frameBuffer;
+// 传输函数
+GLuint g_tffTexObj;
+GLuint g_bfTexObj;
+GLuint g_texWidth;
+GLuint g_texHeight;
+GLuint g_volTexObj;
+GLuint g_rcVertHandle;
+GLuint g_rcFragHandle;
+GLuint g_bfVertHandle;
+GLuint g_bfFragHandle;
+float g_stepSize = 0.01f;
 
 
 int checkForOpenGLError(const char* file, int line)
@@ -35,11 +47,33 @@ int checkForOpenGLError(const char* file, int line)
 	cout << "glError in file " << file
 	     << "@line " << line << gluErrorString(glErr) << endl;
 	retCode = 1;
+	exit(EXIT_FAILURE);
     }
     return retCode;
 }
 void keyboard(unsigned char key, int x, int y);
 void display(void);
+void initVBO();
+void initShader();
+void initFrameBuffer(GLuint, GLuint, GLuint);
+GLuint initTFF1DTex(const char* filename);
+GLuint initFace2DTex();
+GLuint initVol3DTex(const char* filename, GLuint width, GLuint height, GLuint depth);
+void render(GLenum cullFace);
+void init()
+{
+    initVBO();
+    initShader();
+    g_tffTexObj = initTFF1DTex("tff.dat");
+    g_bfTexObj = initFace2DTex();
+    g_volTexObj = initVol3DTex("head256.raw", 256, 256, 225
+);
+    GL_ERROR();
+    g_texWidth = g_winWidth;
+    g_texHeight = g_winHeight;
+    initFrameBuffer(g_bfTexObj, g_texWidth, g_texHeight);
+    GL_ERROR();
+}
 // 初始化顶点缓冲区对象
 void initVBO()
 {
@@ -101,10 +135,10 @@ void initVBO()
     // glBindVertexArray(0);
     g_vao = vao;
 }
-void drawBox()
+void drawBox(GLenum glFaces)
 {
     glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+    glCullFace(glFaces);
     glBindVertexArray(g_vao);
     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, (GLuint *)NULL);
     glDisable(GL_CULL_FACE);
@@ -140,7 +174,7 @@ GLuint initShaderObj(const GLchar* srcfile, GLenum shaderType)
 	exit(EXIT_FAILURE);
     }
     
-    const int MAX_CNT = 1000;
+    const int MAX_CNT = 10000;
     GLchar *shaderCode = (GLchar *) calloc(MAX_CNT, sizeof(GLchar));
     inFile.read(shaderCode, MAX_CNT);
     if (inFile.eof())
@@ -176,7 +210,7 @@ GLuint initShaderObj(const GLchar* srcfile, GLenum shaderType)
     }
     return shader;
 }
-GLboolean checkShaderLinkStatus(GLuint pgmHandle)
+GLint checkShaderLinkStatus(GLuint pgmHandle)
 {
     GLint status;
     glGetProgramiv(pgmHandle, GL_LINK_STATUS, &status);
@@ -195,7 +229,7 @@ GLboolean checkShaderLinkStatus(GLuint pgmHandle)
     return status;
 }
 // link shader program
-GLuint linkShaderObj(GLuint vertShader, GLuint fragShader)
+GLuint createShaderPgm()
 {
     // Create the shader program
     GLuint programHandle = glCreateProgram();
@@ -204,46 +238,295 @@ GLuint linkShaderObj(GLuint vertShader, GLuint fragShader)
 	cerr << "Error create shader program" << endl;
 	exit(EXIT_FAILURE);
     }
-
-    // Bind index 0 to the shader input variable "VerPos"
-    glBindAttribLocation(programHandle, 0, "VerPos");
-    // Bind index 1 to the shader input variable "VerClr"
-    glBindAttribLocation(programHandle, 0, "VerClr");
-
-    // Attach the shaders to the shader program
-    glAttachShader(programHandle, vertShader);
-    glAttachShader(programHandle, fragShader);
-
-    // Link the program
-    glLinkProgram(programHandle);
-    if (GL_FALSE == checkShaderLinkStatus(programHandle))
-    {
-	cerr << "Failed to link shader program!" << endl;
-	exit(EXIT_FAILURE);
-    }
     return programHandle;
 }
-// 初始化着色器程序
 
+
+// 初始化1D纹理对象，用作传输函数
+GLuint initTFF1DTex(const char* filename)
+{
+    // 读入传输函数
+    ifstream inFile(filename, ifstream::in);
+        if (!inFile)
+    {
+	cerr << "Error openning file: " << filename << endl;
+	exit(EXIT_FAILURE);
+    }
+    
+    const int MAX_CNT = 10000;
+    GLubyte *tff = (GLubyte *) calloc(MAX_CNT, sizeof(GLubyte));
+    inFile.read(reinterpret_cast<char *>(tff), MAX_CNT);
+    if (inFile.eof())
+    {
+	size_t bytecnt = inFile.gcount();
+	*(tff + bytecnt) = '\0';
+    }
+    else if(inFile.fail())
+    {
+	cout << filename << "read failed " << endl;
+    }
+    else
+    {
+	cout << filename << "is too large" << endl;
+    }    
+    GLuint tff1DTex;
+    glGenTextures(1, &tff1DTex);
+    glBindTexture(GL_TEXTURE_1D, tff1DTex);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage1D(GL_TEXTURE_1D, 0, GL_RGBA8, 256, 0, GL_RGBA, GL_UNSIGNED_BYTE, tff);
+    free(tff);    
+    return tff1DTex;
+}
+// 初始化2D纹理对象
+GLuint initFace2DTex()
+{
+    GLuint backFace2DTex;
+    glGenTextures(1, &backFace2DTex);
+    glBindTexture(GL_TEXTURE_2D, backFace2DTex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, 512, 512, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    return backFace2DTex;
+}
+// init 3D texture
+GLuint initVol3DTex(const char* filename, GLuint w, GLuint h, GLuint d)
+{
+    
+    FILE *fp;
+    size_t size = w * h * d;
+    GLubyte *data = new GLubyte[size];			  // 8bit
+    if (!(fp = fopen(filename, "rb")))
+    {
+        cout << "Error: opening .raw file failed" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        cout << "OK: open .raw file successed" << endl;
+    }
+    if ( fread(data, sizeof(char), size, fp)!= size) 
+    {
+        cout << "Error: read .raw file failed" << endl;
+        exit(1);
+    }
+    else
+    {
+        cout << "OK: read .raw file successed" << endl;
+    }
+    fclose(fp);
+
+    glGenTextures(1, &g_volTexObj);
+    // bind 3D texture target
+    glActiveTexture(GL_TEXTURE4);
+    glBindTexture(GL_TEXTURE_3D, g_volTexObj);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
+// 启用三线性过滤，以得到更加清晰的图像
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+    // 注意该处涉及到像素传输
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+    glTexImage3D(GL_TEXTURE_3D, 0, GL_INTENSITY, w, h, d, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE,data);
+
+    delete []data;
+    cout << "volume texture created" << endl;
+    return g_volTexObj;
+}
+
+void checkFramebufferStatus()
+{
+    GLenum complete = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (complete != GL_FRAMEBUFFER_COMPLETE)
+    {
+	cout << "framebuffer is not complete" << endl;
+	exit(EXIT_FAILURE);
+    }
+    
+
+}
+// 初始化framebuffer
+void initFrameBuffer(GLuint texObj, GLuint texWidth, GLuint texHeight)
+{
+    // create a depth buffer for our framebuffer
+    GLuint depthBuffer;
+    glGenRenderbuffers(1, &depthBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, texWidth, texHeight);
+
+    // attach the texture and the depth buffer to the framebuffer
+    glGenFramebuffers(1, &g_frameBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, g_frameBuffer);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texObj, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer);
+    checkFramebufferStatus();
+    glEnable(GL_DEPTH_TEST);    
+}
+
+void rcSetUinforms()
+{
+    // 设置步长
+    // 背面纹理
+    // 体纹理
+    GLint stepSizeLoc = glGetUniformLocation(g_programHandle, "StepSize");
+    GL_ERROR();
+    if (stepSizeLoc >= 0)
+    {
+	glUniform1f(stepSizeLoc, g_stepSize);
+    }
+    else
+    {
+	cout << "StepSize"
+	     << "is not bind to the uniform"
+	     << endl;
+    }
+    GL_ERROR();
+    GLint transferFuncLoc = glGetUniformLocation(g_programHandle, "TransferFunc");
+    if (transferFuncLoc >= 0)
+    {
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_1D, g_tffTexObj);
+	glUniform1i(transferFuncLoc, 0);
+    }
+    else
+    {
+	cout << "TransferFunc"
+	     << "is not bind to the uniform"
+	     << endl;
+    }
+    GL_ERROR();    
+    GLint backFaceLoc = glGetUniformLocation(g_programHandle, "ExitPoints");
+    if (backFaceLoc >= 0)
+    {
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, g_bfTexObj);
+	glUniform1i(backFaceLoc, 1);
+    }
+    else
+    {
+	cout << "ExitPoints"
+	     << "is not bind to the uniform"
+	     << endl;
+    }
+    GL_ERROR();    
+    GLint volumeLoc = glGetUniformLocation(g_programHandle, "VolumeTex");
+    if (volumeLoc >= 0)
+    {
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_3D, g_volTexObj);
+	glUniform1i(volumeLoc, 2);
+    }
+    else
+    {
+	cout << "VolumeTex"
+	     << "is not bind to the uniform"
+	     << endl;
+    }
+    
+}
+// 初始化着色器程序
 void initShader()
 {
 // 初始化顶点着色器
-    GLuint vertHandle = initShaderObj("shader/raycasting.vert", GL_VERTEX_SHADER);
+    g_bfVertHandle = initShaderObj("shader/backface.vert", GL_VERTEX_SHADER);
 // 初始化片段着色器
-    GLuint fragHandle = initShaderObj("shader/raycasting.frag", GL_FRAGMENT_SHADER);
+    g_bfFragHandle = initShaderObj("shader/backface.frag", GL_FRAGMENT_SHADER);
+    g_rcVertHandle = initShaderObj("shader/raycasting.vert", GL_VERTEX_SHADER);
+    g_rcFragHandle = initShaderObj("shader/raycasting.frag", GL_FRAGMENT_SHADER);
 // 初始化着色器程序, use it in an appropriate time
-    g_programHandle = linkShaderObj(vertHandle, fragHandle);
+    g_programHandle = createShaderPgm();
 // 获得由着色器编译器分配的索引(可选)
 }
+
+void linkShader(GLuint shaderPgm, GLuint newVertHandle, GLuint newFragHandle)
+{
+    const GLsizei maxCount = 2;
+    GLsizei count;
+    GLuint shaders[maxCount];
+    glGetAttachedShaders(shaderPgm, maxCount, &count, shaders);
+    cout << "get VertHandle: " << shaders[0] << endl;
+    cout << "get FragHandle: " << shaders[1] << endl;
+    GL_ERROR();
+    for (int i = 0; i < count; i++) {
+	glDetachShader(shaderPgm, shaders[i]);
+    }
+    // Bind index 0 to the shader input variable "VerPos"
+    glBindAttribLocation(shaderPgm, 0, "VerPos");
+    // Bind index 1 to the shader input variable "VerClr"
+    glBindAttribLocation(shaderPgm, 1, "VerClr");
+    GL_ERROR();
+    glAttachShader(shaderPgm,newVertHandle);
+    glAttachShader(shaderPgm,newFragHandle);
+    GL_ERROR();
+    glLinkProgram(shaderPgm);
+    if (GL_FALSE == checkShaderLinkStatus(shaderPgm))
+    {
+	cerr << "Failed to relink shader program!" << endl;
+	exit(EXIT_FAILURE);
+    }
+    GL_ERROR();
+}
+
 void display()
 {
     glEnable(GL_DEPTH_TEST);
     // test the gl_error
     checkForOpenGLError(__FILE__, __LINE__);
+    // render to texture
+    // 该处是否需要像素传输？
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, g_frameBuffer);
     glViewport(0, 0, g_winWidth, g_winHeight);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.2f,0.2f,0.2f,1.0f);
+    linkShader(g_programHandle, g_bfVertHandle, g_bfFragHandle);
     glUseProgram(g_programHandle);
+    // cull front face
+    render(GL_FRONT);
+    glUseProgram(0);
+    GL_ERROR();
+    linkShader(g_programHandle, g_rcVertHandle, g_rcFragHandle);
+    GL_ERROR();
+    glUseProgram(g_programHandle);
+    rcSetUinforms();
+    GL_ERROR();
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glViewport(0, 0, g_winWidth, g_winHeight);
+    glUseProgram(g_programHandle);
+    // cull back face
+    render(GL_BACK);
+    // 恢复使用单个纹理
+    glActiveTexture(GL_TEXTURE1);
+    glDisable(GL_TEXTURE_2D);
+    glActiveTexture(GL_TEXTURE2);
+    glDisable(GL_TEXTURE_3D);
+    glActiveTexture(GL_TEXTURE0);
+    glDisable(GL_TEXTURE_1D);
+    glActiveTexture(GL_TEXTURE0);
+    glUseProgram(0);
+    GL_ERROR();    
+    // glBindFramebuffer(GL_READ_FRAMEBUFFER, g_frameBuffer);
+    // checkFramebufferStatus();
+    // glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+    // glViewport(0, 0, g_winWidth, g_winHeight);
+    // glClearColor(0.0, 0.0, 1.0, 1.0);
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // GL_ERROR();
+    // glBlitFramebuffer(0, 0, g_winWidth, g_winHeight,0, 0,
+    // 		      g_winWidth, g_winHeight, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+    // glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    // GL_ERROR();
+    glutSwapBuffers();
+}
+// 如何设计两次都使用render？
+// 第一次渲染正方体的背面
+// 第二次渲染正方体的前面， 使用不一样的着色器程序，其余相同？
+void render(GLenum cullFace)
+{
+    GL_ERROR();
+    glClearColor(0.2f,0.2f,0.2f,1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     //  transform the box
     glm::mat4 projection = glm::perspective(60.0f, (GLfloat)g_winWidth/g_winHeight, 0.1f, 100.f);
     glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f),
@@ -252,7 +535,7 @@ void display()
     glm::mat4 model = mat4(1.0f);
     model *= glm::rotate((float)g_angle, glm::vec3(0.0f, 1.0f, 0.0f));
     model *= glm::translate(glm::vec3(-0.5f, -0.5f, -0.5f)); 
-    // notice the multiplication order
+    // notice the multiplication order: reverse order of transform
     glm::mat4 mvp = projection * view * model;
     GLuint mvpIdx = glGetUniformLocation(g_programHandle, "MVP");
     if (mvpIdx >= 0)
@@ -263,14 +546,23 @@ void display()
     {
     	cerr << "can't get the MVP" << endl;
     }
-    // draw the back face of the box 
+    // draw the back face of the box
+    // the color of the vertex in the back face is also the location
+    // of the vertex
+    // save the back face to the user defined framebuffer bound
+    // with a 2D texture named `backFace`
     // draw the front face of the box
-    checkForOpenGLError(__FILE__, __LINE__);
-    drawBox();
-    checkForOpenGLError(__FILE__, __LINE__);
-    // glUseProgram(0);
+    // in the rendering process, loading the volume `vol` as well as the `backFace`
+    // after vertex shader processing we got the color as well as the location of
+    // the vertex. and the vertex assemblied into primitives before entering
+    // fragment shader processing stage.
+    // in fragment shader processing stage. we got `vol` adn `backFace` as well as
+    // the location of primitives. the most important is that we got the GLSL to
+    // exec the logic. Here we go! 
+    GL_ERROR();
+    drawBox(cullFace);
+    GL_ERROR();
     // glutWireTeapot(0.5);
-    glutSwapBuffers();
 }
 void rotateDisplay()
 {
@@ -281,9 +573,12 @@ void reshape(int w, int h)
 {
     g_winWidth = w;
     g_winHeight = h;
+    g_texWidth = w;
+    g_texHeight = h;
 }
 int main(int argc, char** argv)
 {
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutCreateWindow("GLUT Test");
@@ -294,17 +589,15 @@ int main(int argc, char** argv)
 	/* Problem: glewInit failed, something is seriously wrong. */
 	fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
     }
+  
     glutKeyboardFunc(&keyboard);
     glutDisplayFunc(&display);
     glutReshapeFunc(&reshape);
     glutIdleFunc(&rotateDisplay);
-    initShader();
-    initVBO();
+    init();
     glutMainLoop();
     return EXIT_SUCCESS;
 }
-
-
 void keyboard(unsigned char key, int x, int y)
 {
     switch (key)
--- shader/raycasting.vert
+++ shader/raycasting.vert
@@ -1,16 +1,20 @@
-// for raycasting
 #version 400
 
-layout(location = 0) in vec3 VerPos;
-layout(location = 1) in vec3 VerClr;
 
-out vec3 Color;
+layout (location = 0) in vec3 VerPos;
+// have to use this variable!!!, or it will be very hard to debug for AMD video card
+layout (location = 1) in vec3 VerClr;  
+
+
+out vec3 EntryPoint;
+out vec4 ExitPointCoord;
 
 uniform mat4 MVP;
 
-
 void main()
 {
-    Color = VerClr;
-    gl_Position = MVP * vec4(VerPos, 1.0);
+    EntryPoint = VerClr;
+    gl_Position = MVP * vec4(VerPos,1.0);
+    // ExitPointCoord 输入到fragment shader 的过程中经过rasterization， interpolation, assembly primitive
+    ExitPointCoord = gl_Position;  
 }
